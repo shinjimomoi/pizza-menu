365a8d9d618a95023948d42883d13058c797b095
Ricardo Momoi <shinjimomoi@gmail.com>
2023-12-03 00:10:21 +0900

    Structured the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Menu />
     </div>
   );
+}
+
+function Menu() {
+  return (
+    <>
+      <Header />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Footer />
+    </>
+  )
+}
+
+function Header() {
+  return (
+    <h1>Fast React Pizza CO.</h1>
+  )
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <img src="/pizzas/prosciutto.jpg" alt="focaccia" />
+      <p>Pizza Prosciuto</p>
+    </div>
+  )
+}
+
+function Footer() {
+  const hour = new Date().getHours;
+  const openHours = hour > 10 && hour < 22;
+  const isOpen = openHours ? "open" : "closed";
+  return (
+    <h4>We are {isOpen}</h4>
+  )
 }
 
 export default App;
